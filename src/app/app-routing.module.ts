8eaaae9340728d356ba769ad4677a5643ea354e2
pirate <82434072+hiPirate@users.noreply.github.com>
2021-09-30 11:44:15 -0500

    Updated Site v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { GalleryComponent } from './gallery/gallery.component';
 import { HomeComponent } from './home/home.component';
 import { CharityComponent} from './charity/charity.component';
-import { SocialComponent } from './social/social.component';
 import { AboutComponent } from './about/ about.component';
 
 const routes: Routes = [{ path: '', component: HomeComponent },
 { path: 'gallery', component: GalleryComponent },  
 { path: 'charity', component: CharityComponent },
-{ path: 'about', component: AboutComponent },
-{ path: 'social', component: SocialComponent}];
-
+{ path: 'about', component: AboutComponent };
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,10 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { GalleryComponent } from './gallery/gallery.component';
 import { HomeComponent } from './home/home.component';
 import { CharityComponent} from './charity/charity.component';
-import { SocialComponent } from './social/social.component';
 import { AboutComponent } from './about/ about.component';
 import { FaqComponent } from './faq/faq.component';
-import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {
@@ -28,14 +26,6 @@ const routes: Routes = [
   {
       path: 'faq',
       component: FaqComponent
-  },
-  {
-      path: 'social',
-      component: SocialComponent
-  },
-  {
-      path: 'contact',
-      component: ContactComponent
   }
 ];
 
