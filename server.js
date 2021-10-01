cc75a5eb7d9b164ce17c0b3aecaeab39d30f5c53
pirate <eric@thepirateship.dev>
2021-10-01 18:26:23 -0500

    Add Color Theme Check for Favicon
    When in dark mode, favicon will be white, and when in light mode, favicon will be black. This way it's visible on most devices/themes.

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 // Server for Bermano site to run on Heroku
- function requireHTTPS(req, res, next) {
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-} 
+
 
 const express = require('express');
 const app = express();
 
-app.use(requireHTTPS);
+
 app.use(express.static('./dist/bermano'));
 
 app.get('/*', (req, res) =>
