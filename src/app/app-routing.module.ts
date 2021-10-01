c5307ed3c56ff1a5f588ffaa40e79d9f96cd91a9
pirate <82434072+hiPirate@users.noreply.github.com>
2021-10-01 09:32:18 -0500

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomePage } from '../pages/home/home.component';
+import { CollectionsPage } from '../pages/collections/collections.component';
+import { KotjPage } from '../pages/collections/kotj/kotj.component';
+import { AboutPage } from 'src/pages/about/about.component';
+import { CharityPage } from 'src/pages/charity/charity.component';
+import { FaqPage } from 'src/pages/faq/faq.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomePage },
+  { path: 'collections', component: CollectionsPage },
+  { path: 'collections/kotj', component: KotjPage },
+  { path: 'about', component: AboutPage },
+  { path: 'charity', component: CharityPage },
+  { path: 'faq', component: FaqPage }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MDBBootstrapModule, MdbInput } from 'angular-bootstrap-md';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { Navigation } from '../components/navigation/navigation.component'
+import { Footer } from '../components/footer/footer.component';
+
+import { HomePage } from '../pages/home/home.component'
+import { CollectionsPage } from '../pages/collections/collections.component';
+import { KotjPage } from '../pages/collections/kotj/kotj.component';
+import { AboutPage } from 'src/pages/about/about.component';
+import { CharityPage } from 'src/pages/charity/charity.component';
+import { FaqPage } from 'src/pages/faq/faq.component';
+import { ButtonsModule } from 'angular-bootstrap-md';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Navigation,
+    Footer,
+    HomePage,
+    CollectionsPage,
+    KotjPage,
+    AboutPage,
+    CharityPage,
+    FaqPage
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
+    MdbCheckboxModule,
+    ButtonsModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ]
+})
+export class AppModule { }
